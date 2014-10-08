63218efb5b76fd255bc0bb229197b22de51c3a0e
loliden001 <loliden001@ikasle.ehu.es>
2014-10-08 10:55:49 +0200

    bigarren identifikazio eskaera

--- hauteskundeak/src/com/isad/bertsioak/Kautotu.java
+++ hauteskundeak/src/com/isad/bertsioak/Kautotu.java
@@ -3,7 +3,7 @@ package com.isad.bertsioak;
 public class Kautotu {
 	public void identifikatu(){
 			System.out.println("Zure id behar dugu:");
-
+			System.out.println("Zure id behar dugu berriz:");
 	}
 
 }
